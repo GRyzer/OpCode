d3a839bd943f8cf6f9bb9f4bff84b6515525c560
GRyzer <GRyzer59@gmail.com>
2020-05-10 14:30:38 +0200

    refactor variable names

--- InstructionPtr.py
+++ InstructionPtr.py
@@ -1,15 +1,24 @@
 class InstructionPointer:
-    def __init__(self, number):
-        self.instr_ptr = number
+    def __init__(self, value):
+        self.instr_ptr = value
 
-    def __add__(self, number):
-        self.instr_ptr += number
+    def __add__(self, value):
+        self.instr_ptr += value
 
-    def __sub__(self, number):
-        self.instr_ptr -= number
+    def __sub__(self, value):
+        self.instr_ptr -= value
 
     def __repr__(self):
         return f'{self.instr_ptr}'
 
     def __call__(self):
         return self.instr_ptr
+
+    def set_new_value(self, value):
+        self.instr_ptr = value
+
+
+if __name__ == "__main__":
+    instr = InstructionPointer(0)
+    instr.set_new_value(6)
+    print(instr())
